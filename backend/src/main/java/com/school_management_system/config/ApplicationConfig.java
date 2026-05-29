3d095ac077abcc407662871931c8fa9ae21fd8b6
serventino-servenn <serventinoetom@gmail.com>
2026-05-28 23:01:32 -0400

    fixed the login issue

--- backend/src/main/java/com/school_management_system/config/ApplicationConfig.java
+++ backend/src/main/java/com/school_management_system/config/ApplicationConfig.java
@@ -1,17 +1,8 @@
 package com.school_management_system.config;
 
-
-
-import com.school_management_system.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
